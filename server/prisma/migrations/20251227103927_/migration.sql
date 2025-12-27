6474d6a4e809c0df105781f6fc946970aba1cf80
Eko nur arifin <aryjamers.21@gmail.com>
2025-12-27 17:39:44 +0700

    feat: Migrate database to PostgreSQL with initial User and Message table schema.

--- server/prisma/migrations/20251227103927_/migration.sql
+++ server/prisma/migrations/20251227103927_/migration.sql
@@ -1,23 +1,26 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "online" BOOLEAN NOT NULL DEFAULT false,
-    "lastSeen" DATETIME,
+    "lastSeen" TIMESTAMP(3),
     "sockets" TEXT NOT NULL DEFAULT '[]',
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Message" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "fromUserId" TEXT NOT NULL,
     "toUserId" TEXT NOT NULL,
     "text" TEXT NOT NULL,
     "roomId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "Message_fromUserId_fkey" FOREIGN KEY ("fromUserId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -28,3 +31,6 @@ CREATE INDEX "Message_roomId_createdAt_idx" ON "Message"("roomId", "createdAt");
 
 -- CreateIndex
 CREATE INDEX "Message_fromUserId_idx" ON "Message"("fromUserId");
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_fromUserId_fkey" FOREIGN KEY ("fromUserId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
